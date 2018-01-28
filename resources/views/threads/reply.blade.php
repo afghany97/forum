33effeb7e1b15afb70600599dbdc0042fa1647d7
afghany <afghany@admin.com>
2018-01-28 22:12:43 +0200

    improving the code

---
+++ resources/views/threads/reply.blade.php
@@ -0,0 +1,19 @@
+ <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                
+                    <a href="#"> 
+                        {{$Reply->User->name}}
+                    </a>
+                    From {{$Reply->created_at->diffForHumans()}}
+                        
+                </div>
+
+                <div class="panel-body">
+                        {{$Reply->body}}
+                </div>
+            </div>
+        </div>
+    </div>
+    
