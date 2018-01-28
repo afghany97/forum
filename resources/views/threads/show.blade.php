da908bf31b115040be3bc1a9eff97039959639e1
afghany <afghany@admin.com>
2018-01-28 22:12:13 +0200

    improving the code

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,45 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{$thread->title}}</div>
+    <div class="container">
 
-                <div class="panel-body">
+        <div class="row">
+        
+            <div class="col-md-8 col-md-offset-2">
+        
+                <div class="panel panel-default">
+        
+                    <div class="panel-heading">
+
+                        <a href="#">
+                            {{$thread->User->name}}
+                         </a>
+        
+                         Posted ...
+        
+                        {{$thread->title}}
+                    </div>
                         
-                        <div class="body">
-                            {{$thread->body}}
-                        </div>
-                        
+                    <div class="panel-body">
+                            
+                            <div class="body">
+        
+                                {{$thread->body}}
+        
+                            </div>
+        
+                    </div>
+
                 </div>
+
             </div>
-        </div>
-    </div>
-    
-    @foreach($thread->Replies as $Reply)
-    
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                
-                    <a href="#"> 
-                        {{$Reply->User->name}}
-                    </a>
-                    From {{$Reply->created_at->diffForHumans()}}
-                        
-                </div>
 
-                <div class="panel-body">
-                        {{$Reply->body}}
-                </div>
-            </div>
         </div>
-    </div>
-    
-    @endforeach
+        
+        @foreach($thread->Replies as $Reply)
+        
+            @include('threads.reply')
+        
+        @endforeach
 
-</div>
+    </div>
+
 @endsection
--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ReplyTest extends TestCase
