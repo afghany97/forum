3b6827519bfae97b38a003dd6f35d2058d2546a1
afghany <afghany@admin.com>
2018-02-08 13:29:14 +0200

    modify create thread form and add errors layouts

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app'  )
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -6,12 +6,30 @@
 	<form method = "POST" action="/threads">
 	
 	    {{ csrf_field() }}
+
+
+	    <div class="form-group">
+		
+			<select name="channel_id" class="form-control" required>
+				
+				<option value="">Select Channel</option>
+
+				@foreach(\App\Channel::all() as $channel)
+
+					<option value="{{$channel->id}}" {{$channel->id == old('channel_id') ? 'selected' : ""}}>{{$channel->name}}</option>
+
+				@endforeach
+
+			</select>
+		
+		</div>
+
 	
 	    <div class="form-group">
 		
 			<label for="exampleInputEmail1">thread title</label>
 		
-			<input name = "title" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="post title" required>
+			<input name = "title" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="post title" value="{{old('title')}}" required>
 		
 		</div>
 
@@ -19,14 +37,16 @@
 		
 			<label for="exampleInputPassword1">thread body</label>
 		
-			<textarea name = "body" type="text" class="form-control" id="exampleInputPassword1" placeholder="post body"></textarea>
+			<textarea name = "body" type="text" class="form-control" id="exampleInputPassword1" placeholder="post body" required>{{old('body')}}
+			</textarea>
 		
 		</div>
 		
-			<input name="channel_id" type="hidden" value="1">
 
 		<button type="submit" class="btn btn-primary">create post</button>
-	
+		
+		@include('layouts.errors')
+
 	</form>
 
 </div>
