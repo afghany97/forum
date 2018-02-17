e5747566eadfda610162fde6bcc47afccdfdcc93
afghany <afghany@admin.com>
2018-02-17 18:42:36 +0200

    refactoring favourite's functions to trait

--- app/Reply.php
+++ app/Reply.php
@@ -12,7 +12,7 @@ use App\Thread;
 
 class Reply extends Model
 {
-    use RecordsActivites;
+    use RecordsActivites , ableToFavourite;
 
   	// unuard all fileds of replies table "able to fill"
 
@@ -49,23 +49,5 @@ class Reply extends Model
 
   // create the relationshop betwwen replies and favourites table
 
-  public function favourites()
-  {
-    return $this->morphMany(Favourite::class , 'favorited');
-  }
-
-
-  public function favourite()
-  {
-    // check if this user didn't favourite this reply
-
-    if(! $this->favourites()->where('user_id' , auth()->id())->exists())
-      
-      $this->favourites()->create(['user_id' => auth()->id()]);
-  }
-
-  public function isFavourited()
-  {
-    return !! $this->favourites->where('user_id' , auth()->id())->count();
-  }
+  
 }
---
+++ app/ableToFavourite.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace App;
+
+trait ableToFavourite{
+
+  public function favourites()
+  {
+    return $this->morphMany(Favourite::class , 'favorited');
+  }
+
+
+  public function favourite()
+  {
+    // check if this user didn't favourite this reply
+
+    if(! $this->favourites()->where('user_id' , auth()->id())->exists())
+      
+      $this->favourites()->create(['user_id' => auth()->id()]);
+  }
+
+  public function isFavourited()
+  {
+    return !! $this->favourites->where('user_id' , auth()->id())->count();
+  }
+}
