17abb7e9b362c067ec4e87c62ed4b0a2f8c04a81
Muhammad Reda <muhammadreda97@gmail.com>
2018-09-10 15:59:46 +0200

    give name for routes && install npm dependencies

--- routes/web.php
+++ routes/web.php
@@ -21,71 +21,66 @@ Route::get('/home', function () {
 
 Auth::routes();
 
-Route::post('/threads/{thread}/update/history' , 'ModifyController@show');
+Route::post('/threads/{thread}/update/history' , 'ModifyController@show')->name('thread.history');
 
 Route::get('/threads','ThreadsController@index')->name('threads');
 
-Route::match(['patch','get' , 'put'],'/threads/{channel}/{thread}/edit','ThreadsController@edit');
+Route::match(['patch','get' , 'put'],'/threads/{channel}/{thread}/edit','ThreadsController@edit')->name('thread.edit');
 
-Route::put('/threads/{channel}/{thread}/update','ThreadsController@update');
+Route::put('/threads/{channel}/{thread}/update','ThreadsController@update')->name('thread.update');
 
-Route::put('/threads/{thread}/lock','LockThreadControllers@update');
+Route::put('/threads/{thread}/lock','LockThreadControllers@update')->name('thread.lockToggle');
 
-Route::delete('replies/{reply}/unfavourite' , 'FavouriteController@destroy');
+Route::delete('replies/{reply}/unfavourite' , 'FavouriteController@destroy')->name('reply.unfavourite');
 
-Route::delete('/threads/{thread}/unfavourite' , 'FavouriteController@destroy');
+Route::delete('/threads/{thread}/unfavourite' , 'FavouriteController@destroy')->name('thread.unfavourite');
 
-Route::get('/threads/create' , 'ThreadsController@create');
+Route::get('/threads/create' , 'ThreadsController@create')->name('thread.create');
 
-Route::get('/threads/{channel}','ThreadsController@index');
+Route::get('/threads/{channel}','ThreadsController@index')->name('channel.show');
 
-Route::get('/threads/{channel}/{thread}','ThreadsController@show');
+Route::get('/threads/{channel}/{thread}','ThreadsController@show')->name('thread.show');
 
-Route::delete('/threads/{channel}/{thread}','ThreadsController@destroy');
+Route::delete('/threads/{channel}/{thread}','ThreadsController@destroy')->name('thread.destroy');
 
-Route::post('/threads','ThreadsController@store')->middleware('shoudConfirmYourEmail');
+Route::post('/threads','ThreadsController@store')->middleware('shoudConfirmYourEmail')->name('thread.store');
 
-Route::post('/threads/{channel}/{thread}/replies','RepliesController@store')->middleware('shoudConfirmYourEmail');
+Route::post('/threads/{channel}/{thread}/replies','RepliesController@store')->middleware('shoudConfirmYourEmail')->name('reply.post');
 
-Route::delete('/replies/{reply}','RepliesController@destroy');
+Route::delete('/replies/{reply}','RepliesController@destroy')->name('reply.destroy');
 
-Route::patch('/replies/{reply}','RepliesController@edit');
+Route::patch('/replies/{reply}','RepliesController@edit')->name('reply.edit');
 
-Route::put('/replies/{reply}','RepliesController@update');
+Route::put('/replies/{reply}','RepliesController@update')->name('reply.update');
 
-Route::post('replies/{reply}/favourite' , 'FavouriteController@store');
+Route::post('replies/{reply}/favourite' , 'FavouriteController@store')->name('reply.favourite');
 
-Route::post('threads/{thread}/favourite' , 'FavouriteController@store');
+Route::post('threads/{thread}/favourite' , 'FavouriteController@store')->name('thread.favourite');
 
 Route::get('/profiles/{user}' , 'ProfilesController@show')->name('profile');
 
-Route::post('/threads/{channel}/{thread}/subscribe','SubscribesController@store');
+Route::post('/threads/{channel}/{thread}/subscribe','SubscribesController@store')->name('thread.subscribe');
 
-Route::delete('/threads/{channel}/{thread}/subscribe','SubscribesController@destroy');
+Route::delete('/threads/{channel}/{thread}/subscribe','SubscribesController@destroy')->name('thread.unsubscribe');
 
-Route::delete('/profiles/{user}/notifications/{notification}','NotificationsController@destroy');
+Route::delete('/profiles/{user}/notifications/{notification}','NotificationsController@destroy')->name('user.notifications.destroy');
 
-Route::get('/profiles/{user}/notifications','NotificationsController@index');
+Route::get('/profiles/{user}/notifications','NotificationsController@index')->name('user.notifications');
 
 Route::post('/users/{user}/avatar','AvatarsController@store')->middleware('auth')->name('avatar');
 
-Route::get('/test' , 'ThreadsController@test');
+Route::get('/register/confirm/{token}','UsersControllers@index')->name('user.confirm');
 
-Route::get('/register/confirm/{token}','UsersControllers@index');
+Route::post('/replies/{reply}/best','RepliesController@bestReply')->name('reply.best');
 
-Route::post('/replies/{reply}/best','RepliesController@bestReply');
+Route::get('/admin/control' , 'AdminsController@index')->name('dashboard');
 
-Route::get('/admin/control' , 'AdminsController@index');
+Route::get('/admin/control/users' , 'AdminsController@showUsers')->name('dashboard.users');
 
-Route::get('/admin/control/users' , 'AdminsController@showUsers');
+Route::get('/admin/control/threads' , 'AdminsController@showThreads')->name('dashboard.threads');
 
-Route::get('/admin/control/threads' , 'AdminsController@showThreads');
-
-Route::post('/users/{user}/supervisor' , 'SupervisorController@store');
+Route::post('/users/{user}/supervisor' , 'SupervisorController@store')->name('user.supervisor');
 
 Route::match(['patch','get' , 'put'],'/profiles/{user}/edit' , 'ProfilesController@edit')->name('profile-edit');
 
 Route::put('/profiles/{user}/update' , 'ProfilesController@update')->name('profile-update');
-
-
-Route::get('/test','ThreadsController@test');
